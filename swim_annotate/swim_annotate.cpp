2cdb7f23f00dc70b1d3f4b54b763ff57f5b7aecc
Tim <tjwoinosk@gmail.com>
2019-05-30 12:25:46 -0700

    Uploading project files

---
+++ swim_annotate/swim_annotate.cpp
@@ -0,0 +1,15 @@
+//Tim Woinoski
+//2019-05-30
+// swim_annotate.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//This application labels swimming videos quicker
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
+
+
