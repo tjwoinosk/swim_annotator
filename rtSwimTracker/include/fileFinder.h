b180650033e317ed1b977fb3a1f3c57e095487ab
Tim <twoinosk@sfu.ca>
2021-10-13 09:48:04 -0700

    Updated fileFinder to create file dir if they do not exist

--- rtSwimTracker/include/fileFinder.h
+++ rtSwimTracker/include/fileFinder.h
@@ -19,6 +19,8 @@ public:
 
 private:
 
+	std::string createNewDir(bf::path newDir);
+
 	const std::string testDataLocation = "test\\testData\\";
 	const std::string speedTestDataLocation = "test\\speedTestResults\\";
 
--- rtSwimTracker/src/fileFinder.cpp
+++ rtSwimTracker/src/fileFinder.cpp
@@ -31,11 +31,14 @@ std::string fileFinder::returnDataLocation()
     bf::path testP(testDataLocation);
     bf::path result;
 
-    if (bf::exists(testP))
+    if (bf::is_directory(testP))
     {
         result = bf::absolute(testP);
         return result.string();
     }
+    else
+      return createNewDir(testP);
+
     return std::string();
 }
 
@@ -44,10 +47,28 @@ std::string fileFinder::returnSpeedTestLocation()
     bf::path speedP(speedTestDataLocation);
     bf::path result;
 
-    if (bf::exists(speedP))
+    if (bf::is_directory(speedP))
     {
         result = bf::absolute(speedP);
         return result.string();
     }
+    else
+      return createNewDir(speedP);
+
+    return std::string();
+}
+
+std::string fileFinder::createNewDir(bf::path newDir)
+{
+  bool couldCreate = false;
+  bf::path result;
+
+  couldCreate = create_directories(newDir);
+  if (couldCreate)
+  {
+    result = bf::absolute(newDir);
+    return result.string();
+  }
+  else
     return std::string();
 }
