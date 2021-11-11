cd2705fa49f9d789aa00efdc750d8280b83c4cb7
Kiran <gkbrar@sfu.ca>
2021-11-11 15:08:35 -0800

    added sample test file to add test cases in for GUI

---
+++ SSAGUI/test/TestSSAGUI.cpp
@@ -0,0 +1,9 @@
+#define BOOST_TEST_MODULE mytests
+#include <boost/test/included/unit_test.hpp>
+//Add --detect_memory_leak=0 to debug command to remove memory leak output
+
+BOOST_AUTO_TEST_CASE(myTestCase)
+{
+  BOOST_TEST(1 == 1);
+  BOOST_TEST(true);
+}
