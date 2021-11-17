b306e91d1b15e1c5f31323fd2e8d52bf33d0dcb0
Kiran <gkbrar@sfu.ca>
2021-11-16 16:19:10 -0800

    updated test cases to have one simple cancel test case

--- SSAGUI/test/TestSSAGUI.cpp
+++ SSAGUI/test/TestSSAGUI.cpp
@@ -8,7 +8,6 @@
 
 
 BOOST_AUTO_TEST_SUITE(trackSingleSwimmerTestSuite)
-//BOOST_AUTO_TEST_CASE(testLaneTwoTracking, * boost::unit_test::tolerance(float(1.0)))
 BOOST_AUTO_TEST_CASE(testLaneTwoTracking)
 {
     //For video 14.mp4 the coordinates (x, y) = (299, 325) 
@@ -48,7 +47,7 @@ BOOST_AUTO_TEST_CASE(testLaneTwoTracking)
         BOOST_CHECK_CLOSE(output_box.height, gt_box.height, 5.2);
     }
 }
-BOOST_AUTO_TEST_CASE(testLaneEightTrackingTwo)
+BOOST_AUTO_TEST_CASE(testLaneEightTracking)
 {
     //For video 14.mp4 the coordinates (x, y) = (659 , 83)
     //correspond to the swimmer in the second lane
@@ -72,6 +71,53 @@ BOOST_AUTO_TEST_CASE(testLaneEightTrackingTwo)
     std::ifstream ifs1(outputName);
     std::ifstream ifs2(gtName);
 
+    while (std::getline(ifs1, line_output) && std::getline(ifs2, line_gt))
+    {
+        std::istringstream read_output(line_output);
+        std::istringstream read_gt(line_gt);
+        TrackingBox output_box;
+        TrackingBox gt_box;
+
+        output_box.inputFromFile(read_output);
+        gt_box.inputFromFile(read_gt);
+
+        //BOOST_TEST(output_box == gt_box); //TODO IDs and frame num will not match unless updated
+        BOOST_CHECK_CLOSE(output_box.x, gt_box.x, 1.5); // 1.5 = 1.5%
+        BOOST_CHECK_CLOSE(output_box.y, gt_box.y, 1.6);
+        BOOST_CHECK_CLOSE(output_box.width, gt_box.width, 5.2);
+        BOOST_CHECK_CLOSE(output_box.height, gt_box.height, 5.2);
+    }
+}
+BOOST_AUTO_TEST_CASE(testLaneEightTrackingCancellation)
+{
+    //For video 14.mp4 the coordinates (x, y) = (659 , 83)
+    //correspond to the swimmer in the second lane
+    //near frame number 190
+
+    fileFinder find;
+    std::string outputName = "detectionDataNEW.txt";
+    std::string gtName;
+    std::string line_gt, line_output;
+    SSAGUI ssaVideo("14.mp4");
+    SSAGUI ssaVideoTwo("14.mp4");
+
+    outputName = find.absolutePath(outputName);
+    gtName = outputName;
+    gtName.replace(gtName.end() - 4, gtName.end(), "T2.txt");
+    gtName.replace(gtName.end() - 4, gtName.end(), "GT.txt");
+
+    //ssaVideo.playVideoTest(true, 14, 20, 26, cv::Point_<float>(300, 275));
+    //TODO how do you test cancelled? cuz it won't output anything... should you read the file beforehand, and then see if nothign changed?
+    ssaVideo.playVideoTest(false, 185, 190, 200, cv::Point_<float>(659, 83));
+    std::cout << " ------------TESTING TESTING TESTING ONE end" << std::endl;
+    ssaVideoTwo.playVideoTest(true, 14, 20, 26, cv::Point_<float>(300, 275)); //This should make no changes to the output file
+    std::cout << " ------------TESTING TESTING TESTING TWO end" << std::endl;
+
+    std::ifstream ifs1(outputName);
+    std::ifstream ifs2(gtName);
+
+    //The test is that the due to the order of the tests, the output should remain as 
+    //that of test "testLaneEightTracking" even though we run "testLaneTwoTracking" with cancellation
     while (std::getline(ifs1, line_output) && std::getline(ifs2, line_gt))
     {
         std::istringstream read_output(line_output);
